0af1de5bff5ee398964cbe82eacc0f1180a1c6e5
saipradeep484 <saipradeep484@gmail.com>
2024-08-13 20:37:28 +0530

    gdfsdf

---
+++ src/main/java/com/example/dileep/controller/BasicController.java
@@ -0,0 +1,13 @@
+package com.example.dileep.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+    @GetMapping("endpoint")
+    public String method1()
+    {
+        return "hello meet my amigo dileep";
+    }
+}
---
+++ src/test/java/com/example/dileep/DileepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dileep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DileepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
